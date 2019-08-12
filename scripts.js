9a3e5cd54339a8e14b431372fd7ded115cf64769
Ricardo <ricardo@apretaste.org>
2019-08-12 15:02:53 -0500

    Back to black, ES5 rewrite

--- scripts.js
+++ scripts.js
@@ -1,193 +1,238 @@
+"use strict";
+
 var optionsModalActive = false;
 var moved = false;
 var activeChat;
 var activeMessage;
 var activeUsername;
 var timer;
+$(function () {
+  if (typeof messages != "undefined") {
+    /*resizeChat();
+    $(window).resize(() => resizeChat());
+    $('.chat').scrollTop($('.bubble:last-of-type').offset().top);*/
+    $('#message').focus();
+    activeChat = id;
+    activeUsername = username;
+    $('.bubble').on("touchstart", function (event) {
+      runTimer();
+      activeMessage = event.currentTarget.id;
+    }).on("touchmove", function (event) {
+      clearTimeout(timer);
+      moved = true;
+    }).on("touchend", function (event) {
+      clearTimeout(timer);
+    });
+    $('.bubble').on("mousedown", function (event) {
+      runTimer();
+      activeMessage = event.currentTarget.id;
+    }).on("mouseup", function (event) {
+      clearTimeout(timer);
+    });
+  }
 
-$(() => {
-    if (typeof messages != "undefined") {
-		/*resizeChat();
-		$(window).resize(() => resizeChat());
-		$('.chat').scrollTop($('.bubble:last-of-type').offset().top);*/
-        $('#message').focus();
-        activeChat = id;
-        activeUsername = username;
-
-        $('.bubble')
-            .on("touchstart", event => { runTimer(); activeMessage = event.currentTarget.id; })
-            .on("touchmove", event => { clearTimeout(timer); moved = true; })
-            .on("touchend", event => { clearTimeout(timer); });
-
-        $('.bubble')
-            .on("mousedown", event => { runTimer(); activeMessage = event.currentTarget.id; })
-            .on("mouseup", event => {clearTimeout(timer);});
-    }
-    
-    $('.modal').modal();
-    $('.openchat')
-        .on("touchstart", event => { runTimer(); activeChat = event.currentTarget.id; activeUsername = event.currentTarget.getAttribute('username'); })
-        .on("touchmove", event => { clearTimeout(timer); moved = true; })
-        .on("touchend", event => { openChat() });
-
-    $('.openchat')
-        .on("mousedown", event => { runTimer(); activeChat = event.currentTarget.id; activeUsername = event.currentTarget.getAttribute('username'); })
-        .on("mouseup", event => { openChat() });
+  $('.modal').modal();
+  $('.openchat').on("touchstart", function (event) {
+    runTimer();
+    activeChat = event.currentTarget.id;
+    activeUsername = event.currentTarget.getAttribute('username');
+  }).on("touchmove", function (event) {
+    clearTimeout(timer);
+    moved = true;
+  }).on("touchend", function (event) {
+    openChat();
+  });
+  $('.openchat').on("mousedown", function (event) {
+    runTimer();
+    activeChat = event.currentTarget.id;
+    activeUsername = event.currentTarget.getAttribute('username');
+  }).on("mouseup", function (event) {
+    openChat();
+  });
 });
 
 function openChat() {
-    if (!optionsModalActive && !moved) apretaste.send({ 'command': 'CHAT', 'data': { 'userId': activeChat } });
-    optionsModalActive = false;
-    moved = false;
-    clearTimeout(timer);
+  if (!optionsModalActive && !moved) apretaste.send({
+    'command': 'CHAT',
+    'data': {
+      'userId': activeChat
+    }
+  });
+  optionsModalActive = false;
+  moved = false;
+  clearTimeout(timer);
 }
 
 function viewProfile() {
-    apretaste.send({ 'command': 'PERFIL', 'data': { 'username': activeChat } });
+  apretaste.send({
+    'command': 'PERFIL',
+    'data': {
+      'username': activeChat
+    }
+  });
 }
 
 function writeModalOpen() {
-    optionsModalActive = false;
-    M.Modal.getInstance($('#optionsModal')).close();
-    M.Modal.getInstance($('#writeMessageModal')).open();
+  optionsModalActive = false;
+  M.Modal.getInstance($('#optionsModal')).close();
+  M.Modal.getInstance($('#writeMessageModal')).open();
 }
 
 function deleteModalOpen() {
-    optionsModalActive = false;
-    M.Modal.getInstance($('#optionsModal')).close();
-    if(typeof messages == "undefined") $('#deleteModal p').html('¿Esta seguro de eliminar su chat con @'+ activeUsername +'?');
-    M.Modal.getInstance($('#deleteModal')).open();
+  optionsModalActive = false;
+  M.Modal.getInstance($('#optionsModal')).close();
+  if (typeof messages == "undefined") $('#deleteModal p').html('¿Esta seguro de eliminar su chat con @' + activeUsername + '?');
+  M.Modal.getInstance($('#deleteModal')).open();
 }
 
 function resendModalOpen() {
-    optionsModalActive = false;
-    M.Modal.getInstance($('#optionsModal')).close();
-    M.Modal.getInstance($('#resendMessageModal')).open();
+  optionsModalActive = false;
+  M.Modal.getInstance($('#optionsModal')).close();
+  M.Modal.getInstance($('#resendMessageModal')).open();
 }
 
-function resendMessage(){
-    var username = $('#usernameToResend').val().trim();
-    var message = $('#'+activeMessage+' msg').html()
-    if(username.length > 2){
-        apretaste.send({
-            'command':'CHAT ESCRIBIR',
-            'data':{'id': username, 'message': message},
-            'redirect': false,
-            'callback': {'name':'resendMessageCallback','data':username}
-        })
-    }
-    else showToast("Ingrese un username valido")
-    
-}
+function resendMessage() {
+  var username = $('#usernameToResend').val().trim();
+  var message = $('#' + activeMessage + ' msg').html();
 
-function searchProfile(){
-    var username = $('#usernameToSearch').val().trim();
-    if(username.length > 2){
-        apretaste.send({
-            'command':'CHAT BUSCAR',
-            'data':{'username': username}
-        })
-    }
-    else showToast("Ingrese un username valido")
-    
-}
-
-function deleteChat(){
+  if (username.length > 2) {
     apretaste.send({
-        'command': 'CHAT BORRAR',
-        'data':{'id':activeChat, 'type': 'chat'},
-        'redirect': false,
-        'callback':{'name':'deleteChatCallback','data':activeChat}
-    })
+      'command': 'CHAT ESCRIBIR',
+      'data': {
+        'id': username,
+        'message': message
+      },
+      'redirect': false,
+      'callback': {
+        'name': 'resendMessageCallback',
+        'data': username
+      }
+    });
+  } else showToast("Ingrese un username valido");
 }
 
-function deleteMessage(){
+function searchProfile() {
+  var username = $('#usernameToSearch').val().trim();
+
+  if (username.length > 2) {
     apretaste.send({
-        'command': 'CHAT BORRAR',
-        'data':{'id':activeMessage, 'type': 'message'},
-        'redirect': false,
-        'callback':{'name':'deleteMessageCallback','data':activeMessage}
-    })
+      'command': 'CHAT BUSCAR',
+      'data': {
+        'username': username
+      }
+    });
+  } else showToast("Ingrese un username valido");
 }
 
-function deleteChatCallback(chatId){
-    $('#'+chatId).remove();
-    showToast('Chat eliminado');
+function deleteChat() {
+  apretaste.send({
+    'command': 'CHAT BORRAR',
+    'data': {
+      'id': activeChat,
+      'type': 'chat'
+    },
+    'redirect': false,
+    'callback': {
+      'name': 'deleteChatCallback',
+      'data': activeChat
+    }
+  });
 }
 
-function deleteMessageCallback(messageId){
-    $('#'+messageId).remove();
-    showToast('Mensaje eliminado');
+function deleteMessage() {
+  apretaste.send({
+    'command': 'CHAT BORRAR',
+    'data': {
+      'id': activeMessage,
+      'type': 'message'
+    },
+    'redirect': false,
+    'callback': {
+      'name': 'deleteMessageCallback',
+      'data': activeMessage
+    }
+  });
 }
 
-function resendMessageCallback(username){
-    $('#usernameToResend').val('');
-    showToast("Mensaje reenviado a @"+username);
+function deleteChatCallback(chatId) {
+  $('#' + chatId).remove();
+  showToast('Chat eliminado');
+}
+
+function deleteMessageCallback(messageId) {
+  $('#' + messageId).remove();
+  showToast('Mensaje eliminado');
+}
+
+function resendMessageCallback(username) {
+  $('#usernameToResend').val('');
+  showToast("Mensaje reenviado a @" + username);
 }
 
 function runTimer() {
-    timer = setTimeout(function () {
-        optionsModalActive = true;
-        M.Modal.getInstance($('#optionsModal')).open();
-    }, 800);
+  timer = setTimeout(function () {
+    optionsModalActive = true;
+    M.Modal.getInstance($('#optionsModal')).open();
+  }, 800);
 }
 
 function sendMessage() {
-    var message = $('#message').val().trim();
-    if (message.length > 0) {
-        apretaste.send({
-            'command': "CHAT ESCRIBIR",
-            'data': { 'id': activeChat, 'message': message },
-            'redirect': false,
-            'callback': { 'name': 'sendMessageCallback', 'data': message }
-        });
-    }
-    else showToast("Mensaje vacio");
+  var message = $('#message').val().trim();
+
+  if (message.length > 0) {
+    apretaste.send({
+      'command': "CHAT ESCRIBIR",
+      'data': {
+        'id': activeChat,
+        'message': message
+      },
+      'redirect': false,
+      'callback': {
+        'name': 'sendMessageCallback',
+        'data': message
+      }
+    });
+  } else showToast("Mensaje vacio");
 }
 
 function sendMessageCallback(message) {
-    if (typeof messages != "undefined") {
-        if (messages.length == 0) {
-            $('#nochats').remove();
-            $('#messageField').insertBefore("<div class=\"chat\"></div>");
-        }
+  if (typeof messages != "undefined") {
+    if (messages.length == 0) {
+      $('#nochats').remove();
+      $('#messageField').insertBefore("<div class=\"chat\"></div>");
+    }
 
-        $('.chat').append(
-            "<div class=\"bubble me\" id=\"last\">" +
-            "<small>" +
-            "    <b>@" + myusername + "</b> - " + new Date(Date.now()).toLocaleString() +
-            "</small>" +
-            "<br>" +
-            message +
-            "</div>"
-        );
-    }
-    else{
-        if(message.length > 70) message = message.substr(0, 70)+'...';
-        $('#'+activeChat+' msg').html(message)
-    }
-    $('#message').val('')
+    $('.chat').append("<div class=\"bubble me\" id=\"last\">" + "<small>" + "    <b>@" + myusername + "</b> - " + new Date(Date.now()).toLocaleString() + "</small>" + "<br>" + message + "</div>");
+  } else {
+    if (message.length > 70) message = message.substr(0, 70) + '...';
+    $('#' + activeChat + ' msg').html(message);
+  }
+
+  $('#message').val('');
 }
 
 function messageLengthValidate() {
-    var message = $('#message').val().trim();
-    if (message.length <= 500) {
-        $('.helper-text').html('Restante: ' + (500 - message.length));
-    }
-    else {
-        $('.helper-text').html('Limite excedido');
-    }
+  var message = $('#message').val().trim();
+
+  if (message.length <= 500) {
+    $('.helper-text').html('Restante: ' + (500 - message.length));
+  } else {
+    $('.helper-text').html('Limite excedido');
+  }
 }
 
-function resizeChat(){
-	if($('.row').length == 3){
-		$('.chat').height($(window).height() - $($('.row')[0]).outerHeight(true) - $($('.row')[2]).outerHeight(true));
-	}
+function resizeChat() {
+  if ($('.row').length == 3) {
+    $('.chat').height($(window).height() - $($('.row')[0]).outerHeight(true) - $($('.row')[2]).outerHeight(true));
+  }
 
-	$('.chat').height($('.chat').height() - $('#messageField').outerHeight(true) - $('h3').outerHeight(true) - 20)
-	if(online=="0") $('.chat').height($('.chat').height() - $('small').outerHeight());
+  $('.chat').height($('.chat').height() - $('#messageField').outerHeight(true) - $('h3').outerHeight(true) - 20);
+  if (online == "0") $('.chat').height($('.chat').height() - $('small').outerHeight());
 }
 
 function showToast(text) {
-    M.toast({ html: text });
+  M.toast({
+    html: text
+  });
 }
+
