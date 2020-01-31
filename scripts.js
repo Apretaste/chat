6eb108d006c7db390457f4b81c2b249ffc5855f7
salvipascual <salvi.pascual@gmail.com>
2020-01-31 18:09:23 +0100

    add search section

--- scripts.js
+++ scripts.js
@@ -1,367 +1,121 @@
-"use strict";
-
-var optionsModalActive = false;
-var moved = false;
-var activeChat;
-var activeMessage;
-var activeUsername;
-var timer;
-
-var colors = {
-	'Azul': '#99F9FF',
-	'Verde': '#9ADB05',
-	'Rojo': '#FF415B',
-	'Morado': '#58235E',
-	'Naranja': '#F38200',
-	'Amarillo': '#FFE600'
-};
-
-var avatars = {
-	'Rockera': 'F',
-	'Tablista': 'F',
-	'Rapero': 'M',
-	'Guapo': 'M',
-	'Bandido': 'M',
-	'Encapuchado': 'M',
-	'Rapear': 'M',
-	'Inconformista': 'M',
-	'Coqueta': 'F',
-	'Punk': 'M',
-	'Metalero': 'M',
-	'Rudo': 'M',
-	'Señor': 'M',
-	'Nerd': 'M',
-	'Hombre': 'M',
-	'Cresta': 'M',
-	'Emo': 'M',
-	'Fabulosa': 'F',
-	'Mago': 'M',
-	'Jefe': 'M',
-	'Sensei': 'M',
-	'Rubia': 'F',
-	'Dulce': 'F',
-	'Belleza': 'F',
-	'Músico': 'M',
-	'Rap': 'M',
-	'Artista': 'M',
-	'Fuerte': 'M',
-	'Punkie': 'M',
-	'Vaquera': 'F',
-	'Modelo': 'F',
-	'Independiente': 'F',
-	'Extraña': 'F',
-	'Hippie': 'M',
-	'Chica Emo': 'F',
-	'Jugadora': 'F',
-	'Sencilla': 'F',
-	'Geek': 'F',
-	'Deportiva': 'F',
-	'Moderna': 'F',
-	'Surfista': 'M',
-	'Señorita': 'F',
-	'Rock': 'F',
-	'Genia': 'F',
-	'Gótica': 'F',
-	'Sencillo': 'M',
-	'Hawaiano': 'M',
-	'Ganadero': 'M',
-	'Gótico': 'M'
-};
-
-$(function () {
+$(document).ready(function() {
+	// initialize components
 	$('.tabs').tabs();
-	if (typeof messages != "undefined") {
-		resizeChat();
-		$(window).resize(function () {
-			return resizeChat();
-		});
-		if (messages.length > 0) $('.chat').scrollTop($('.bubble:last-of-type').offset().top);
-
-		$('#message').focus();
-		activeChat = id;
-		activeUsername = username;
-		$('.bubble').on("touchstart", function (event) {
-			runTimer();
-			activeMessage = event.currentTarget.id;
-		}).on("touchmove", function (event) {
-			clearTimeout(timer);
-			moved = true;
-		}).on("touchend", function (event) {
-			clearTimeout(timer);
-		});
-		$('.bubble').on("mousedown", function (event) {
-			runTimer();
-			activeMessage = event.currentTarget.id;
-		}).on("mouseup", function (event) {
-			clearTimeout(timer);
-		});
-	}
-
 	$('.modal').modal();
-	$('.openchat').on("touchstart", function (event) {
-		activeChat = event.currentTarget.id;
-		activeUsername = event.currentTarget.getAttribute('username');
-	})
-	$('.openchat').on("mousedown", function (event) {
-		activeChat = event.currentTarget.id;
-		activeUsername = event.currentTarget.getAttribute('username');
-	})
+	$('select').formSelect();
 
-	$(document).ready(function () {
-		$('.fixed-action-btn').floatingActionButton({
-			'direction': 'left',
-			'hoverEnabled': false,
-
-		});
-	});
+	// scroll to the bottom if in the chat page
+	var isAtChatPage = $('#chat-row').length == 1;
+	if(isAtChatPage) scrollToEndOfPage();
 });
 
-function resizeChat() {
-	$('.chat').height($(window).height() - $('#messageField').outerHeight(true) - 20);
-}
-
-function openChat() {
-	if (!optionsModalActive && !moved) {
-		apretaste.send({
-			'command': 'CHAT',
-			'data': {
-				'userId': activeChat
-			}
-		});
-	}
-	optionsModalActive = false;
-	moved = false;
-	clearTimeout(timer);
-}
-
-function getAvatar(avatar, serviceImgPath, size) {
-	var index = Object.keys(avatars).indexOf(avatar);
-	var fullsize = size * 7;
-	var x = index % 7 * size;
-	var y = Math.floor(index / 7) * size;
-	return "background-image: url(" + serviceImgPath + "/avatars.png);" + "background-size: " + fullsize + "px " + fullsize + "px;" + "background-position: -" + x + "px -" + y + "px;";
-}
-
-function viewProfile() {
+function chat(id) {
 	apretaste.send({
-		'command': 'PERFIL',
-		'data': {
-			'username': activeChat
-		}
+		'command': 'CHAT',
+		'data': {'userId': id}
 	});
 }
 
-function writeModalOpen() {
-	optionsModalActive = false;
-	M.Modal.getInstance($('#optionsModal')).close();
-	M.Modal.getInstance($('#writeMessageModal')).open();
+function profile(username) {
+	apretaste.send({
+		'command': 'PERFIL',
+		'data': {'username': username}
+	});
 }
 
-function deleteModalOpen() {
-	if (typeof messages == "undefined") {
-		$('#deleteModal p').html('¿Eliminar chat con @' + activeUsername + '?');
-	}
+function deleteModalOpen(id, username) {
+	// add to id to the modal so the modal knows what to delete
+	$('#deleteModal').attr('data-value', id);
+
+	// change the modal's message
+	$('#deleteModalUsername').html(username);
+
+	// open the modal
 	M.Modal.getInstance($('#deleteModal')).open();
 }
 
-function resendModalOpen() {
-	optionsModalActive = false;
-	M.Modal.getInstance($('#optionsModal')).close();
-	M.Modal.getInstance($('#resendMessageModal')).open();
-}
-
-function resendMessage() {
-	var username = $('#usernameToResend').val().trim();
-	var message = $('#' + activeMessage + ' msg').html();
-
-	if (username.length > 2) {
-		apretaste.send({
-			'command': 'CHAT ESCRIBIR',
-			'data': {
-				'id': username,
-				'message': message
-			},
-			'redirect': false,
-			'callback': {
-				'name': 'resendMessageCallback',
-				'data': username
-			}
-		});
-	} else {
-		showToast("Ingrese un username valido");
-	}
-}
-
-function searchProfile() {
-	var username = $('#usernameToSearch').val().trim();
-
-	if (username.length > 2) {
-		apretaste.send({
-			'command': 'CHAT BUSCAR',
-			'data': {
-				'username': username
-			}
-		});
-	} else {
-		showToast("Ingrese un username valido");
-	}
-}
-
 function deleteChat() {
+	// get id from the modal
+	var id = $('#deleteModal').attr('data-value');
+
+	// delete the chat
 	apretaste.send({
 		'command': 'CHAT BORRAR',
-		'data': {
-			'id': activeChat,
-			'type': 'chat'
-		},
+		'data': {'id': id, 'type': 'chat'},
 		'redirect': false,
-		'callback': {
-			'name': 'deleteChatCallback',
-			'data': activeChat
-		}
+		'callback': {'name':'deleteChatCallback', 'data':id}
 	});
 }
 
-function deleteMessage() {
+function searchUsers() {
+	// get values from the form
+	var username = $('#username').val();
+	var province = $('#province').val();
+	var gender = $('#gender').val();
+	var min_age = $('#min_age').val() * 1;
+	var max_age = $('#max_age').val() * 1;
+	var religion = $('#religion').val();
+
+	// check the age range is valid
+	if(min_age < 0 || max_age > 120 || min_age > max_age) {
+		M.toast({html: 'Error en la edad'});
+		return false;
+	}
+
+	// send request to the backend
 	apretaste.send({
-		'command': 'CHAT BORRAR',
+		'command': "CHAT USERS",
 		'data': {
-			'id': activeMessage,
-			'type': 'message'
-		},
-		'redirect': false,
-		'callback': {
-			'name': 'deleteMessageCallback',
-			'data': activeMessage
+			'username': username,
+			'province': province,
+			'gender': gender,
+			'min_age': min_age,
+			'max_age': max_age,
+			'religion': religion
 		}
 	});
 }
 
 function deleteChatCallback(chatId) {
 	$('#' + chatId).remove();
-	showToast('Chat eliminado');
+	M.toast({html: 'Chat eliminado'});
 }
 
-function deleteMessageCallback(messageId) {
-	$('#' + messageId).remove();
-	showToast('Mensaje eliminado');
-}
-
-function resendMessageCallback(username) {
-	$('#usernameToResend').val('');
-	showToast("Mensaje reenviado a @" + username);
-}
-
-function runTimer() {
-	timer = setTimeout(function () {
-		optionsModalActive = true;
-		M.Modal.getInstance($('#optionsModal')).open();
-	}, 800);
-}
-
-function sendMessage() {
+function sendMessage(toId) {
+	// get the message to send
 	var message = $('#message').val().trim();
 
-	if (message.length > 0) {
-		apretaste.send({
-			'command': "CHAT ESCRIBIR",
-			'data': {
-				'id': activeChat,
-				'message': message
-			},
-			'redirect': false,
-			'callback': {
-				'name': 'sendMessageCallback',
-				'data': message
-			}
-		});
-	} else {
-		showToast("Mensaje vacio");
-	}
-}
-
-function sendMessageCallback(message) {
-	if (typeof messages != "undefined") {
-		if (messages.length == 0) {
-			// Jquery Bug, fixed in 1.9, insertBefore or After deletes the element and inserts nothing
-			// $('#messageField').insertBefore("<div class=\"chat\"></div>");
-			$('#nochats').remove();
-			$('#chat-row').append("<div class=\"chat\"></div>");
-		}
-
-		$('.chat').append("<div class=\"bubble me\" id=\"last\">" + message + "<br>" + "<small>" + new Date().toLocaleString('es-ES') + "</small>" + "</div>");
-	} else {
-		if (message.length > 70) message = message.substr(0, 70) + '...';
-		$('#' + activeChat + ' msg').html(message);
+	// do now allow short or empty messages
+	if (message.length <= 3) {
+		M.toast({html: "Mínimo 3 letras"});
+		return false;
 	}
 
-	$('#message').val('');
-}
-
-function messageLengthValidate() {
-	var message = $('#message').val().trim();
-
-	if (message.length <= 500) {
-		$('.helper-text').html('Restante: ' + (500 - message.length));
-	} else {
-		$('.helper-text').html('Limite excedido');
-	}
-}
-
-function showToast(text) {
-	M.toast({
-		html: text
+	// send the message
+	apretaste.send({
+		'command': "CHAT ESCRIBIR",
+		'data': {'id':toId, 'message':message},
+		'redirect': false,
+		'callback': {'name':'sendMessageCallback', 'data':message}
 	});
 }
 
-function _typeof(obj) {
-	if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") {
-		_typeof = function _typeof(obj) {
-			return typeof obj;
-		};
-	} else {
-		_typeof = function _typeof(obj) {
-			return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj;
-		};
-	}
-	return _typeof(obj);
+function sendMessageCallback(message) {
+	// remove the no chats message
+	$('#nochats').remove();
+
+	// add a new chat bubble
+	$('.chat').append('<div class="bubble me" id="last">' + message + '<br><small>' + new Date().toLocaleString('es-ES') + '</small></div>');
+
+	// clean the text box
+	$('#message').val('');
+
+	// scroll to the end of the page
+	scrollToEndOfPage();
 }
 
-if (!Object.keys) {
-	Object.keys = function () {
-		'use strict';
-
-		var hasOwnProperty = Object.prototype.hasOwnProperty,
-			hasDontEnumBug = !{
-				toString: null
-			}.propertyIsEnumerable('toString'),
-			dontEnums = ['toString', 'toLocaleString', 'valueOf', 'hasOwnProperty', 'isPrototypeOf', 'propertyIsEnumerable', 'constructor'],
-			dontEnumsLength = dontEnums.length;
-		return function (obj) {
-			if (_typeof(obj) !== 'object' && (typeof obj !== 'function' || obj === null)) {
-				throw new TypeError('Object.keys called on non-object');
-			}
-
-			var result = [],
-				prop,
-				i;
-
-			for (prop in obj) {
-				if (hasOwnProperty.call(obj, prop)) {
-					result.push(prop);
-				}
-			}
-
-			if (hasDontEnumBug) {
-				for (i = 0; i < dontEnumsLength; i++) {
-					if (hasOwnProperty.call(obj, dontEnums[i])) {
-						result.push(dontEnums[i]);
-					}
-				}
-			}
-
-			return result;
-		};
-	}();
+function scrollToEndOfPage() {
+	$("html, body").animate({
+		scrollTop: $(document).height()
+	}, 1000);
 }
-
--- service.php
+++ service.php
@@ -7,13 +7,14 @@ use Apretaste\Notifications;
 use Apretaste\Person;
 use Apretaste\Request;
 use Apretaste\Response;
+use Framework\Core;
 use Framework\Alert;
 use Framework\Database;
 
 class Service
 {
 	/**
-	 * Get the list of conversations, or post a note
+	 * redirect to chat or the list of users
 	 *
 	 * @param Request $request
 	 * @param Response $response
@@ -24,39 +25,209 @@ class Service
 	 */
 	public function _main(Request $request, Response $response)
 	{
-		if (isset($request->input->data->userId)) {
-			// get the username of the note
-			$user = Person::find($request->input->data->userId);
-
-			// check if the username is valid
-			if (!$user) {
-				$response->setLayout('chat.ejs');
-				return $response->setTemplate('notFound.ejs');
-			}
-
-			// get and display messages
-			$chats = Chats::conversation($request->person->id, $user->id);
-
-			$content = [
-				'messages' => $chats,
-				'username' => $user->username,
-				'myuser' => $request->person->id,
-				'id' => $user->id,
-				'online' => $user->isOnline,
-				'gender' => $user->gender,
-				'last' => date('d/m/Y h:i a', strtotime($user->lastAccess))
-			];
-
-			$response->setLayout('chat.ejs');
-			return $response->setTemplate('chat.ejs', $content);
+		// get the list of open chats
+		if (empty($request->input->data->userId)) {
+			return $this->_open($request, $response);
 		}
 
+		// chat with a user
+		return $this->_chat($request, $response);
+	}
+
+	/**
+	 * Get the list of open conversations
+	 *
+	 * @param Request $request
+	 * @param Response $response
+	 * @return
+	 * @throws Alert
+	 * @throws Exception
+	 * @author salvipascual
+	 */
+	public function _open(Request $request, Response $response)
+	{
 		// get the list of people chatting with you
 		$chats = Chats::open($request->person->id);
 
+		// get content for the view
+		$content = [
+			'chats' => $chats, 
+			'myuser' => $request->person->id];
+
 		// send data to the view
-		$response->setLayout('chat.ejs');
-		$response->setTemplate('main.ejs', ['chats' => $chats, 'myuser' => $request->person->id]);
+		$response->setCache('hour');
+		$response->setTemplate('open.ejs', $content);
+	}
+
+	/**
+	 * Search for a user
+	 *
+	 * @param Request $request
+	 * @param Response $response
+	 * @throws Alert
+	 * @author ricardo@apretaste.org
+	 */
+	public function _search(Request $request, Response $response)
+	{
+		// get content for the view
+		$content = [
+			'gender' => Core::$gender, 
+			'religions' => Core::$religions, 
+			'provinces' => Core::$provinces]; 
+
+		// send data to the view
+		$response->setCache('year');
+		$response->setTemplate('search.ejs', $content);
+	}
+
+	/**
+	 * Show the list of users online
+	 *
+	 * @param Request $request
+	 * @param Response $response
+	 * @throws Alert
+	 */
+	public function _online(Request $request, Response $response)
+	{
+		// get users who are online
+		$online = Chats::online($request->person->id);
+
+		// send info to the view
+		$response->setCache('hour');
+		$response->setTemplate('online.ejs', ['users' => $online]);
+	}
+
+	/**
+	 * Display the list of users found
+	 *
+	 * @param Request $request
+	 * @param Response $response
+	 * @throws Alert
+	 * @author ricardo@apretaste.org
+	 */
+	public function _users(Request $request, Response $response)
+	{
+		// get data from the request 
+		$username = $request->input->data->username;
+		$province = $request->input->data->province;
+		$gender = $request->input->data->gender;
+		$min_age = $request->input->data->min_age;
+		$max_age = $request->input->data->max_age;
+		$religion = $request->input->data->religion;
+
+		// declare variables
+		$tags = []; $where = "";
+
+		// search only by @username
+		if($username) {
+			$where = "AND username = '$username'";
+			$tags[] = "@$username";
+		}
+		// if the @username was not passed
+		else {
+			if($gender) {
+				$tags[] = Core::$gender[$gender];
+				$where .= "AND gender = '$gender' ";
+			}
+
+			if($min_age) {
+				$tags[] = "< $min_age años";
+				$year = date('Y') - $min_age;
+				$where .= "AND year_of_birth <= $year ";
+			}
+
+			if($max_age) {
+				$tags[] = "> $max_age años";
+				$year = date('Y') - $max_age;
+				$where .= "AND year_of_birth >= $year ";
+			}
+
+			if($province) {
+				$tags[] = Core::$provinces[$province];
+				$where .= "AND province = '$province' ";
+			}
+
+			if($religion) {
+				$tags[] = Core::$religions[$religion]['name'];
+				$where .= "AND religion = '$religion'";
+			}
+		}
+
+		// search for users
+		$users = Database::query("
+			SELECT id, username, avatar, avatarColor, gender, online
+			FROM person 
+			WHERE active = 1 $where
+			ORDER BY online DESC, last_access DESC
+			LIMIT 24");
+
+		// error if no users were found
+		if (empty($users)) {
+			return $response->setTemplate('message.ejs', [
+				'header' => 'No hay usuarios',
+				'icon' => 'sentiment_neutral',
+				'text' => 'Lo sentimos, pero no encontramos ningún usuario con esa combinación de datos. Cambie los paramétros de búsqueda e intente nuevamente.',
+				'button' => ['href' => 'CHAT SEARCH', 'caption' => 'Buscar']
+			]);
+		}
+
+		// add max reach tag
+		if(count($users) == 24) {
+			$tags[] = "Primeros 24";
+		}
+
+		// get content for the view
+		$content = [
+			'users' => $users, 
+			'tags' => $tags];
+
+		// send data to the view
+		$response->setCache();
+		$response->setTemplate('users.ejs', $content);
+	}
+
+	/**
+	 * Get the list of conversations
+	 *
+	 * @param Request $request
+	 * @param Response $response
+	 * @return
+	 * @throws Alert
+	 * @throws Exception
+	 * @author salvipascual
+	 */
+	public function _chat(Request $request, Response $response)
+	{
+		// ensure a person Id is passed
+		if (empty($request->input->data->userId)) {
+			$response->setCache();
+			return $response->setTemplate('message.ejs', [
+				'header' => 'Usuario inexistente',
+				'icon' => 'sentiment_neutral',
+				'text' => 'Lo sentimos, el usuario que usted busca no existe. Puede que halla dejado de usar la app. Busque otro usuario y comience a chatear.',
+				'button' => ['href' => 'CHAT SEARCH', 'caption' => 'Buscar']
+			]);
+		}
+
+		// get the username of the note
+		$user = Person::find($request->input->data->userId);
+
+		// get and display messages
+		$chats = Chats::conversation($request->person->id, $user->id);
+
+		// get content for the view
+		$content = [
+			'messages' => $chats,
+			'username' => $user->username,
+			'myuser' => $request->person->id,
+			'id' => $user->id,
+			'online' => $user->isOnline,
+			'gender' => $user->gender,
+			'last' => date('d/m/Y h:i a', strtotime($user->lastAccess))
+		];
+
+		// send data to the view
+		$response->setTemplate('chat.ejs', $content);
 	}
 
 	/**
@@ -75,31 +246,10 @@ class Service
 		if ($deleteType === 'chat') {
 			Chats::hide($request->person->id, $idToHide);
 		}
+
 		if ($deleteType === 'message') {
 			Chats::hideMessage($request->person->id, $idToHide);
 		}
-	}
-
-	/**
-	 * Search an user by username
-	 *
-	 * @param Request $request
-	 * @param Response $response
-	 * @return
-	 * @throws Alert
-	 * @author ricardo@apretaste.org
-	 */
-	public function _buscar(Request $request, Response $response)
-	{
-		$username = $request->input->data->username;
-		$user = Person::find($username);
-		if (!$user) {
-			$response->setLayout('chat.ejs');
-			return $response->setTemplate('notFound.ejs');
-		}
-
-		$request->input->data->userId = $user->id;
-		$this->_main($request, $response);
 	}
 
 	/**
@@ -112,58 +262,34 @@ class Service
 	 */
 	public function _escribir(Request $request, Response $response)
 	{
-		if (!isset($request->input->data->id)) return;
+		if (!isset($request->input->data->id)) {
+			return;
+		}
 
 		$userTo = Person::find($request->input->data->id);
-		if (!$userTo) return;
-
-		$message = $request->input->data->message;
+		if (!$userTo) {
+			return;
+		}
 
 		$blocks = Chats::isBlocked($request->person->id, $userTo->id);
 		if ($blocks->blocked > 0 || $blocks->blockedByMe > 0) {
-			Notifications::alert(
-				$request->person->id,
-				"Su mensaje para @{$userTo->username} no pudo ser entregado, es posible que usted haya sido bloqueado por esa persona.",
-				'error',
-				"{'command':'PERFIL', 'data':{'id':'{$userTo->id}'}"
-			);
+			$text .= "Su mensaje para @{$userTo->username} no pudo ser entregado, es posible que usted haya sido bloqueado por esa persona.";
+			Notifications::alert($request->person->id, $text, 'error', "{'command':'PERFIL', 'data':{'id':'{$userTo->id}'}");
 			return;
 		}
 
 		// store the note in the database
-		$message = Database::escape($message, 499);
+		$message = Database::escape($request->input->data->message, 499);
 		Database::query("INSERT INTO _note (from_user, to_user, `text`) VALUES ({$request->person->id},{$userTo->id},'$message')");
 
 		// send notification for the app
-		Notifications::alert(
-			$userTo->id,
-			"@{$request->person->username} le ha enviado una nota",
-			'message',
-			"{'command':'CHAT', 'data':{'id':'{$request->person->id}'}}"
-		);
+		$text .= "@{$request->person->username} le ha enviado una nota";
+		Notifications::alert($userTo->id, $text, 'message', "{'command':'CHAT', 'data':{'id':'{$request->person->id}'}}");
 
 		// complete challenge
 		Challenges::complete("chat", $request->person->id);
 
 		// add the experience
 		Level::setExperience('START_CHAT_FIRST', $request->person->id, $userTo->username);
-	}
-
-	/**
-	 * Sub-service ONLINE
-	 *
-	 * @param Request $request
-	 * @param Response $response
-	 * @return void
-	 * @throws Alert
-	 */
-	public function _online(Request $request, Response $response)
-	{
-		$online = Chats::online($request->person->id);
-
-		// send info to the view
-		$response->setCache(5);
-		$response->setLayout('chat.ejs');
-		$response->setTemplate('online.ejs', ['users' => $online]);
 	}
 }
