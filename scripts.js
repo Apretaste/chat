22abc822c9527428f8b3865d92741a278874a9b4
Ricardo Jimenez <jimenezricardo98@gmail.com>
2020-09-02 06:24:04 -0500

    change upload msg

--- scripts.js
+++ scripts.js
@@ -34,7 +34,7 @@ var messagePicture = null;
 
 function sendFile(base64File) {
 	if (base64File.length > 2584000) {
-		showToast("Imagen demasiado pesada");
+		showToast("La imagen que escogió pesa mucho. Una solución rápida es tomar una captura de pantalla de la imagen, para disminuir el peso sin perder calidad.");
 		$('input:file').val(null);
 		return false;
 	}
@@ -267,7 +267,6 @@ function addFriend(message) {
 }
 
 function addFriendCallback(data) {
-	console.log(data)
 	showToast(data.message);
 	$('#' + data.id).remove();
 }
